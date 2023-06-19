8e4f059dc86472d1bb4a877eb417df88dc106922
Linbojun99 <384131437@qq.com>
2023-06-19 15:28:14 +0800

    your-commit-message

--- R/plot_SSB.R
+++ R/plot_SSB.R
@@ -8,14 +8,20 @@
 #' @param se_color Character. The color of the confidence interval ribbon. Default is "blue".
 #' @param se_alpha Numeric. The transparency of the confidence interval ribbon. Default is 0.2.
 #' @param se Logical. Whether to calculate and plot standard error as confidence intervals. Default is FALSE.
+#' @param type Character. It specifies which the SSB is plot for "SSB" , "SBL" . Default is "SSB".
+#' @param facet_ncol Number of columns in facet wrap. Default is 3.
+#' @param facet_scales Scales for facet wrap. Default is "free".
 #' @return A ggplot object representing the plot.
 #' @export
 #' @examples
 #' \dontrun{
 #' plot_SSB(model_result, line_size = 1.2, line_color = "red", line_type = "solid")
 #' }
-plot_SSB <- function(model_result, line_size = 1.2, line_color = "red", line_type = "solid", se = FALSE, se_color = "red", se_alpha = 0.2){
-  # Extract the SSB data
+plot_SSB <- function(model_result, line_size = 1.2, line_color = "red", line_type = "solid", se = FALSE, se_color = "red", se_alpha = 0.2,type=c("SSB","SBL"),facet_ncol = 3, facet_scales = "free"){
+ if(type=="SSB"){
+
+
+   # Extract the SSB data
   SSB <- model_result[["report"]][["SSB"]]
 
   # Make sure it's a data frame
@@ -57,7 +63,37 @@ plot_SSB <- function(model_result, line_size = 1.2, line_color = "red", line_typ
       ggplot2::labs(y = "SSB", x = "Year", title = "SSB Over Years with Confidence Intervals") +
       ggplot2::theme_minimal()
   }
+ }
+  if(type=="SBL"){
+    # Extract the SBL data
+    SBL <- model_result[["report"]][["SBL"]]
 
+    # Make sure it's a matrix
+    if(!is.matrix(SBL)){
+      SBL <- as.matrix(SBL)
+    }
+
+    # Create Year variable from column names of SBL (assuming columns are years)
+    colnames(SBL) <- as.character(model_result[["year"]])
+    Year <- model_result[["year"]]
+
+    # Create LengthGroup variable from row names of SBL
+    LengthGroup <- paste0("Length bin ", seq_len(nrow(SBL)))
+    LengthGroup <- factor(paste("Length bin ", seq_len(nrow(SBL))), levels=paste("Length bin ", seq_len(nrow(SBL))))
+
+
+    # Convert matrix to data frame in long format
+    SBL_long <- reshape2::melt(SBL)
+    colnames(SBL_long) <- c("LengthGroup", "Year", "Count")
+    SBL_long$LengthGroup <- LengthGroup[as.numeric(SBL_long$LengthGroup)]
+
+    # Plot SBL over the years using ggplot2
+    p <- ggplot2::ggplot(SBL_long, aes(x = Year, y = Count)) +
+      ggplot2::geom_line( size = line_size, color = line_color, linetype = line_type) +
+      ggplot2::facet_wrap(~LengthGroup, ncol = facet_ncol, scales = facet_scales) +
+      ggplot2::labs(x = "Year", y = "Relative abundance", title = "SBL Over Years") +
+      ggplot2::theme_minimal()
+  }
   return(p)
 }
 
--- R/plot_biomass.R
+++ R/plot_biomass.R
@@ -8,13 +8,18 @@
 #' @param se_color Character. The color of the confidence interval ribbon. Default is "blue".
 #' @param se_alpha Numeric. The transparency of the confidence interval ribbon. Default is 0.2.
 #' @param se Logical. Whether to calculate and plot standard error as confidence intervals. Default is FALSE.
+#' @param facet_ncol Integer, number of columns in facet_wrap.
+#' @param facet_scales Character, scales for facet_wrap.
+#' @param type Character. It specifies which the biomass is plot for "B" , "BL" . Default is "N".
 #' @return A ggplot object representing the plot.
 #' @export
 #' @examples
 #' \dontrun{
 #' plot_biomass(model_result, line_size = 1.2, line_color = "red", line_type = "solid")
 #' }
-plot_biomass <- function(model_result, line_size = 1.2, line_color = "red", line_type = "solid", se = FALSE, se_color = "red", se_alpha = 0.2){
+plot_biomass <- function(model_result, line_size = 1.2, line_color = "red", line_type = "solid", se = FALSE, se_color = "red", se_alpha = 0.2,type=c("B","BL"),facet_ncol = 3, facet_scales = "free"){
+  if(type=="B"){
+
   # Extract the biomass data
   biomass <- model_result[["report"]][["B"]]
 
@@ -57,7 +62,41 @@ if(!se)
       ggplot2::labs(y = "Biomass", x = "Year", title = "Biomass Over Years with Confidence Intervals") +
       ggplot2::theme_minimal()
   }
+  }
+  if (type=="BL"){
 
+    # Extract the BL data
+    BL <- model_result[["report"]][["BL"]]
+
+    # Make sure it's a matrix
+    if(!is.matrix(BL)){
+      BL <- as.matrix(BL)
+    }
+
+    # Create Year variable from column names of BL (assuming columns are years)
+    Year <- model_result[["year"]]
+
+    # Create LengthGroup variable from row names of BL
+    LengthGroup <- paste0("Length bin ", seq_len(nrow(BL)))
+    LengthGroup <- factor(paste("Length bin ", seq_len(nrow(BL))), levels=paste("Length bin ", seq_len(nrow(BL))))
+
+    # Convert matrix to data frame in long format
+    BL_long <- reshape2::melt(BL)
+    colnames(BL_long) <- c("LengthGroup", "Year", "Count")
+    BL_long$Year <- Year[match(BL_long$Year, 1:length(Year))]
+    BL_long$LengthGroup <- LengthGroup[as.numeric(BL_long$LengthGroup)]
+
+
+
+
+    # Plot BL over the years using ggplot2
+    p <- ggplot2::ggplot(BL_long, aes(x = Year, y = Count)) +
+      ggplot2::geom_line( size = line_size, color = line_color, linetype = line_type) +
+      ggplot2::facet_wrap(~LengthGroup, ncol = facet_ncol, scales = facet_scales) +
+      ggplot2::labs(x = "Year", y = "Relative abundance", title = "BL Over Years") +
+      ggplot2::theme_minimal()
+
+  }
   return(p)
 }
 
